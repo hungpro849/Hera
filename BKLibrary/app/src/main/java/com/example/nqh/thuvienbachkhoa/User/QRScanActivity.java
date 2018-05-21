a0158902333389216cc6d3778ebca14c21d63bb3
1510486 <1510486@hcmut.edu.vn>
2018-05-21 14:37:24 +0700

    Added: QRCode for return book

---
+++ BKLibrary/app/src/main/java/com/example/nqh/thuvienbachkhoa/User/QRScanActivity.java
@@ -0,0 +1,68 @@
+package com.example.nqh.thuvienbachkhoa.User;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+
+import com.example.nqh.thuvienbachkhoa.R;
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.MultiFormatWriter;
+import com.google.zxing.WriterException;
+import com.google.zxing.common.BitMatrix;
+import com.journeyapps.barcodescanner.BarcodeEncoder;
+
+
+public class  QRScanActivity extends Activity {
+    ImageView qr_img;
+    Bundle mBundle;
+    Intent mIntent;
+    String transaction_id;
+    Button mCheckQR, mBack;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qrscan);
+        qr_img = (ImageView) findViewById(R.id.qr_code_img);
+        mCheckQR = (Button) findViewById(R.id.btn_check_qr);
+        mBack = (Button) findViewById(R.id.btn_back);
+
+        mIntent = getIntent();
+        mBundle = mIntent.getExtras();
+        transaction_id = mBundle.getString("transaction_id");
+        if(transaction_id.equals("")) {
+
+        }
+        else {
+            try {
+                MultiFormatWriter multiFormatWriter = new MultiFormatWriter();
+                BitMatrix bitMatrix = multiFormatWriter.encode(transaction_id,
+                        BarcodeFormat.QR_CODE,800,800);
+                BarcodeEncoder barcodeEncoder = new BarcodeEncoder();
+                Bitmap bitmap = barcodeEncoder.createBitmap(bitMatrix);
+                qr_img.setImageBitmap(bitmap);
+            }catch(WriterException e){
+
+                e.printStackTrace();
+            }
+        }
+        mCheckQR.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //onBackPressed();
+            }
+        });
+        mBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+               onBackPressed();
+            }
+        });
+    }
+
+}
