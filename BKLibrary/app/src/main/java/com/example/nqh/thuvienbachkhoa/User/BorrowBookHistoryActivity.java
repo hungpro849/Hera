19374af062963b1bc81b73007acf0b7ea7b371a8
Tai Tran <1413394@hcmut.edu.vn>
2018-05-21 13:02:14 +0700

    fix: BorrowBookHistoryActivity.java

--- BKLibrary/app/src/main/java/com/example/nqh/thuvienbachkhoa/User/BorrowBookHistoryActivity.java
+++ BKLibrary/app/src/main/java/com/example/nqh/thuvienbachkhoa/User/BorrowBookHistoryActivity.java
@@ -1,10 +1,8 @@
 package com.example.nqh.thuvienbachkhoa.User;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
-
-import com.example.nqh.thuvienbachkhoa.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,10 +16,10 @@ public class BorrowBookHistoryActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_borrowed_book);
+        /*setContentView(R.layout.activity_borrowed_book);
 
 
-        mRecyclerView = (RecyclerView) findViewById(R.id.history_recycler_view);
+        mRecyclerView = (RecyclerView) findViewById(R.id.history_recycler_view);*/
     }
 
 
